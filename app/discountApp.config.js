70947bc4ca726e880463002ebcdabd6e5e01e4a0
Mikhail Mikhalev <Mikhail Mikhalev>
2016-09-09 21:23:14 +0300

    Setting gh-pages del files

--- app/discountApp.config.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    'use strict';
-    angular
-        .module('app')
-        .config(['$resourceProvider', function($resourceProvider){
-            $resourceProvider.defaults.stripTrailingSlashes = false;
-        }]);
-})();
